7aed671554a0cae7ef15fa9ffb8c8fa2ede0bdfb
Naufal Fadhlil <nfw010805@gmail.com>
2025-10-27 23:30:05 +0700

    Create Room.java

---
+++ Tugas 9/Room.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class Room {
+    private String description;
+    private HashMap<String, Room> exits;
+
+    public Room(String description) {
+        this.description = description;
+        exits = new HashMap<>();
+    }
+
+    public void setExit(String direction, Room neighbor) {
+        exits.put(direction, neighbor);
+    }
+
+    public Room getExit(String direction) {
+        return exits.get(direction);
+    }
+
+    public String getShortDescription() {
+        return description;
+    }
+
+    public String getLongDescription() {
+        return "You are " + description + ".\n" + getExitString();
+    }
+
+    private String getExitString() {
+        String returnString = "Exits:";
+        for (String exit : exits.keySet()) {
+            returnString += " " + exit;
+        }
+        return returnString;
+    }
+}
